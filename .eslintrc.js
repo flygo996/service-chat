be6cff13baec97fe36dc8d8cd3caa72c95c64fba
pigeon <1390124183@qq.com>
2020-07-15 17:33:34 +0800

    build: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,17 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb'],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
+    'no-underscore-dangle': 0,
+    'no-unused-vars': 1,
+    'object-curly-newline': 0,
+    semi: [2, 'never'],
+    'arrow-parens': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
-};
+}
